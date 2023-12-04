69e85280adcc17e5868d22507949cb274f61dbbe
Cyrus Zheng <zhengc@email.sc.edu>
2023-12-03 23:43:58 -0500

    completed assignment17

--- public/script.js
+++ public/script.js
@@ -15,7 +15,7 @@ const showTeams = async () => {
 
     teams.forEach((team) => {
         const section = document.createElement("section");
-        section.classList.add("soccer-team");
+        section.classList.add("team");
         teamsDiv.append(section);
 
         const a = document.createElement("a");
@@ -25,6 +25,10 @@ const showTeams = async () => {
         const h3 = document.createElement("h3");
         h3.innerHTML = team.name;
         a.append(h3);
+
+        const img = document.createElement("img");
+        img.src = team.logo;
+        section.append(img);
 
         a.onclick = (e) => {
             e.preventDefault();
@@ -51,7 +55,7 @@ const showTeams = async () => {
             e.preventDefault();
             const confirmation = window.confirm("Are you sure you want to delete this team?");
             if (confirmation) {
-                await deleteTeam(team._id);
+                await deleteTeam(team);
             }
         };
     });
@@ -69,18 +73,13 @@ const displayDetails = (team) => {
 
     const p1 = document.createElement("p");
     teamDetails.append(p1);
-    p1.innerHTML = 'Coach: ' + team.coach;
+    p1.innerHTML = 'Founded: ' + team.founded;
     p1.classList.add("pad-this");
 
     const p2 = document.createElement("p");
     teamDetails.append(p2);
-    p2.innerHTML = 'Stadium: ' + team.stadium;
+    p2.innerHTML = 'Country: ' + team.country;
     p2.classList.add("pad-this");
-
-    const p3 = document.createElement("p");
-    teamDetails.append(p3);
-    p3.innerHTML = 'Country: ' + team.country;
-    p3.classList.add("pad-this");
 
     const ul = document.createElement("ul");
     teamDetails.append(ul);
@@ -100,21 +99,39 @@ const displayDetails = (team) => {
         e.preventDefault();
         document.querySelector(".dialog").classList.remove("transparent");
         document.getElementById("add-edit").innerHTML = "Edit Team Details";
-        populateEditForm(team);
     };
+
+    populateEditForm(team);
+};
+
+const deleteTeam = async (team) => {
+    let response = await fetch(`/api/teams/${team._id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+      });
+    
+      if (response.status != 200) {
+        console.log("error deleting");
+        return;
+      }
+    
+      let result = await response.json();
+      showTeams();
+      document.getElementById("team-details").innerHTML = "";
+      resetForm();
 };
 
 const populateEditForm = (team) => {
     const form = document.getElementById("team-form");
+    console.log(team._id);
     form._id.value = team._id;
     form.name.value = team.name;
-    form.coach.value = team.coach;
-    form.stadium.value = team.stadium;
+    form.founded.value = team.founded;
     form.country.value = team.country;
 
-
     document.getElementById("player-boxes").innerHTML = "";
-
 
     team.players.forEach((player) => {
         const input = document.createElement("input");
@@ -124,24 +141,21 @@ const populateEditForm = (team) => {
     });
 };
 
-const addTeam = async (e) => {
+const addEditTeam = async (e) => {
     e.preventDefault();
     const form = document.getElementById("team-form");
     const formData = new FormData(form);
     const dataStatus = document.getElementById("data-status");
     let response;
+    formData.append("players", getPlayers());
 
     if (form._id.value == -1) {
-
         formData.delete("_id");
-        formData.append("players", getPlayers());
-
         response = await fetch("/api/teams", {
             method: "POST",
             body: formData
         });
     } else {
-
         response = await fetch(`/api/teams/${form._id.value}`, {
             method: "PUT",
             body: formData
@@ -158,28 +172,15 @@ const addTeam = async (e) => {
         return;
     }
 
-    response = await response.json();
+    team = await response.json();
+
+    if (form._id.value != -1) {
+        displayDetails(team);
+    }
+
     resetForm();
     document.querySelector(".dialog").classList.add("transparent");
     showTeams();
-};
-
-const deleteTeam = async (teamId) => {
-    try {
-        const response = await fetch(`/api/teams/${teamId}`, {
-            method: "DELETE"
-        });
-
-        if (response.status !== 200) {
-            console.error("Error deleting team");
-            return;
-        }
-
-        const updatedTeams = await response.json();
-        showTeams(updatedTeams);
-    } catch (error) {
-        console.error(error);
-    }
 };
 
 const getPlayers = () => {
@@ -196,7 +197,7 @@ const getPlayers = () => {
 const resetForm = () => {
     const form = document.getElementById("team-form");
     form.reset();
-    form._id = "-1";
+    form._id.value = "-1";
     document.getElementById("player-boxes").innerHTML = "";
 };
 
@@ -213,11 +214,11 @@ const addPlayer = (e) => {
     const input = document.createElement("input");
     input.type = "text";
     section.append(input);
-}
+};
 
 window.onload = () => {
     showTeams();
-    document.getElementById("team-form").onsubmit = addTeam;
+    document.getElementById("team-form").onsubmit = addEditTeam;
     document.getElementById("add-link").onclick = showHideAdd;
 
     document.querySelector(".close").onclick = () => {
--- server.js
+++ server.js
@@ -6,176 +6,131 @@ app.use(express.static("public"));
 app.use(express.json());
 const cors = require("cors");
 app.use(cors());
+const mongoose = require("mongoose");
 
 const upload = multer({ dest: __dirname + "/public/images" });
+
+mongoose
+  .connect(
+    "mongodb+srv://sattiabicsce:myuofsc3$@cluster0.5ito1rb.mongodb.net/"
+  )
+  .then(() => console.log("Connected to mongodb..."))
+  .catch((err) => console.error("could not connect ot mongodb...", err));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-let soccerTeams = [
-    {
-        _id: 1,
-        name: "FC Barcelona",
-        coach: "Xavi Hernandez",
-        stadium: "Camp Nou",
-        country: "Spain",
-        players: [
-            "Lionel Messi",
-            "Ansu Fati",
-            "Sergio Busquets",
-            "Gerard Piqué",
-            "Jordi Alba"
-        ]
-    },
-    {
-        _id: 2,
-        name: "Real Madrid CF",
-        coach: "Carlo Ancelotti",
-        stadium: "Santiago Bernabeu",
-        country: "Spain",
-        players: [
-            "Karim Benzema",
-            "Vinicius Junior",
-            "Luka Modric",
-            "Sergio Ramos",
-            "Marcelo"
-        ]
-    },
-    {
-        _id: 3,
-        name: "Liverpool FC",
-        coach: "Jurgen Klopp",
-        stadium: "Anfield",
-        country: "England",
-        players: [
-            "Mohamed Salah",
-            "Sadio Mane",
-            "Jordan Henderson",
-            "Virgil van Dijk",
-            "Andrew Robertson"
-        ]
-    },
-    {
-        _id: 4,
-        name: "FC Bayern Munich",
-        coach: "Julian Nagelsmann",
-        stadium: "Allianz Arena",
-        country: "Germany",
-        players: [
-            "Robert Lewandowski",
-            "Thomas Muller",
-            "Joshua Kimmich",
-            "Manuel Neuer",
-            "Alphonso Davies"
-        ]
-    },
-    {
-        _id: 5,
-        name: "Paris Saint-Germain",
-        coach: "Mauricio Pochettino",
-        stadium: "Parc des Princes",
-        country: "France",
-        players: [
-            "Kylian Mbappe",
-            "Neymar Jr.",
-            "Marco Verratti",
-            "Marquinhos",
-            "Achraf Hakimi"
-        ]
-    },
-    {
-        _id: 6,
-        name: "Juventus FC",
-        coach: "Massimiliano Allegri",
-        stadium: "Allianz Stadium",
-        country: "Italy",
-        players: [
-            "Cristiano Ronaldo",
-            "Paulo Dybala",
-            "Giorgio Chiellini",
-            "Federico Chiesa",
-            "Dejan Kulusevski"
-        ]
-    }
-];
-app.get("/api/teams", (req, res) => {
-    res.send(soccerTeams);
+const carSchema = new mongoose.Schema({
+    make: String,
+    model: String,
+    year: Number,
+    type: String,
+    features: [String],
+    img: String,
+    _id: Number
 });
 
-app.post("/api/teams", upload.single("img"), (req, res) => {
-    const result = validateTeam(req.body);
+const Car = mongoose.model("Car", carSchema);
+
+app.get("/api/cars", (req, res) => {
+    getCars(res);
+});
+  
+const getCars = async (res) => {
+    const cars = await Car.find();
+    res.send(cars);
+};
+
+app.post("/api/cars", upload.single("img"), async (req, res) => {
+    console.log(req.body);
+    const result = validateCar(req.body);
 
     if (result.error) {
         res.status(400).send(result.error.details[0].message);
         return;
     }
 
-    const team = {
-        _id: soccerTeams.length + 1,
-        name: req.body.name,
-        coach: req.body.coach,
-        stadium: req.body.stadium,
-        country: req.body.country,
-        players: req.body.players.split(","),
+    // Hack to find new ID\
+    const cars = await Car.find();
+    console.log(cars);
+    let maxID = 0;
+    for (x = 0; x < cars.length; x++) {
+        if (cars[x]._id > maxID) {
+            maxID = cars[x]._id;
+        }
     }
+    maxID++;
 
-    soccerTeams.push(team);
-    res.send(soccerTeams);
-});
-
-app.put("/api/teams/:id", upload.single("img"), (req, res) => {
-    const teamId = parseInt(req.params.id);
-    const teamIndex = soccerTeams.findIndex(team => team._id === teamId);
-
-    if (teamIndex === -1) {
-        res.status(404).send("Team not found");
-        return;
-    }
-
-    const result = validateTeam(req.body);
-
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
-
-    soccerTeams[teamIndex] = {
-        _id: teamId,
-        name: req.body.name,
-        coach: req.body.coach,
-        stadium: req.body.stadium,
-        country: req.body.country,
-        players: req.body.players.split(","),
-    };
-
-    res.send(soccerTeams);
-});
-
-app.delete("/api/teams/:id", (req, res) => {
-    const teamId = parseInt(req.params.id);
-    const teamIndex = soccerTeams.findIndex(team => team._id === teamId);
-
-    if (teamIndex === -1) {
-        res.status(404).send("Team not found");
-        return;
-    }
-
-    soccerTeams.splice(teamIndex, 1);
-    res.send(soccerTeams);
-});
-
-const validateTeam = (team) => {
-    const schema = Joi.object({
-        _id: Joi.optional(),
-        name: Joi.string().min(1).required(),
-        coach: Joi.string().required(),
-        stadium: Joi.string().required(),
-        country: Joi.string().required(),
-        players: Joi.allow(""),
+    const car = new Car({
+        _id: maxID,  
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        type: req.body.type,
+        features: req.body.features.split(","),
     });
 
-    return schema.validate(team);
+    if (req.file) {
+        car.img = "images/" + req.file.filename;
+    }
+
+    createCar(car, res);
+});
+
+const createCar = async (car, res) => {
+    const result = await car.save();
+    res.send(car);
+};
+
+app.put("/api/cars/:id", upload.single("img"), (req, res) => {
+    const result = validateCar(req.body);
+    
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    updateCar(req, res);
+});        
+
+const updateCar = async (req, res) => {
+    let fieldsToUpdate = {
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        type: req.body.type,
+        features: req.body.features.split(","),
+    };
+
+    if (req.file) {
+        fieldsToUpdate.img = "images/" + req.file.filename;
+    }
+
+    const result = await Car.updateOne({ _id: req.params.id }, fieldsToUpdate);
+    const car = await Car.findById(req.params.id);
+    res.send(car);
+};
+
+app.delete("/api/cars/:id", upload.single("img"), (req, res) => {
+    removeCar(res, req.params.id);
+});
+
+const removeCar = async (res, id) => {
+    const car = await Car.findByIdAndDelete(id);
+    res.send(car);
+  };
+
+const validateCar = (car) => {
+    const schema = Joi.object({
+        _id: Joi.allow(""),
+        make: Joi.string().required(),
+        model: Joi.string().required(),
+        year: Joi.number().integer().required(),
+        type: Joi.string().required(),
+        features: Joi.allow(""),
+    });
+
+    return schema.validate(car);
 };
 
 app.listen(3000, () => {
